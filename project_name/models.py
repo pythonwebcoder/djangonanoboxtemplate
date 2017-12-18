a630a2a29f224b5891688f948bb461edba407724
Tim Clemans <pythonwebcoderdotcom@gmail.com>
2017-12-18 13:07:11 -0800

    init

---
+++ project_name/models.py
@@ -0,0 +1,4 @@
+from django.contrib.gis.db import models
+from django.contrib.postgres.operations import CreateExtension
+from django.db import migrations
+
---
+++ project_name/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, JsonResponse
+import settings
+import requests
